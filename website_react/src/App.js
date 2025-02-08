5a00d217d50e3be60489a497d8002bfe07b61901
47813 <vanguard0956@gmail.com>
2025-02-08 18:58:18 +0900

    Modified Settings for TailwindCSS and ROM

--- website_react/src/App.js
+++ website_react/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Notification from './components/notification/Notification';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Edit from './components/Edit';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/notification" element={<Notification />} />\
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/edit" element={<Edit />}></Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
