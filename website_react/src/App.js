bd8a513d4393807ed45505ad8335b9fc025000ba
hyeals2 <hyeals2@naver.com>
2025-02-08 18:32:54 +0900

    Update notice page

--- website_react/src/App.js
+++ website_react/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {/* 상단 */}
+      <div className='top'>
+        <p className="emoji">📣</p>
+        <p className="text">공지사항</p>
+        <p className="writing">｜ 글쓰기 ｜</p>
+      </div>
+      
+      {/* 진행 중인 공지사항 */}
+      <div className="noticeContainer">
+        <div className="notice">
+          <div className="imageContainer">
+            <img className="noticeImage" src="/bg.jpeg" />
+          </div>
+          <div className="textContainer">
+            <p className="main-title">겨울방학 스터디 신청</p>
+            <p className="main-date">(~2024.00.00)</p>
+          </div>
+        </div>
+
+        <div className="notice">
+          <div className="imageContainer">
+            <img className="noticeImage" src="/bg.jpeg" />
+          </div>
+          <div className="textContainer">
+            <p className="main-title">겨울방학 스터디 신청</p>
+            <p className="main-date">(~2024.00.00)</p>
+          </div>
+        </div>
+
+      </div>
+
+      {/* 지난 게시물 */}
+      <div className="listContainer">
+        <div className="list">
+          <p className="title">2024-2 스터디 신청</p>
+          <p className="date">(~2024.00.00)</p>
+        </div>
+        <hr className="divider" />
+
+        <div className="list">
+          <p className="title">2024-2 기말고사 열품타 이벤트</p>
+          <p className="date">(~2024.00.00)</p>
+        </div>
+        <hr className="divider" />
+
+        <div className="list">
+          <p className="title">2024-2 빼빼로데이 이벤트</p>
+          <p className="date">(~2024.00.00)</p>
+        </div>
+    </div>
+
     </div>
   );
 }
